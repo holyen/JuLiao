90db580d6cd7ae6cb6846f9d390ac0b3972f312b
zou.hongyuan <holyen86@qq.com>
2014-09-11 14:07:24 +0800

    code init

---
+++ JuLiao/JuLiao/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JuLiao
+//
+//  Created by holyenzou on 14-9-11.
+//  Copyright (c) 2014年 0jiedian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JDAppDelegate class]));
+    }
+}
---
+++ JuLiao/JuLiaoTests/JuLiaoTests.m
@@ -0,0 +1,34 @@
+//
+//  JuLiaoTests.m
+//  JuLiaoTests
+//
+//  Created by holyenzou on 14-9-11.
+//  Copyright (c) 2014年 0jiedian. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JuLiaoTests : XCTestCase
+
+@end
+
+@implementation JuLiaoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
